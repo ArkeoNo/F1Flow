01a3fbb8443fc912cd13c335d8071b6aee3027d7
Arthuro <arthur.nogues@gmail.com>
2022-03-15 23:20:52 +0100

    Création de la branche data analyse Modif  du gitignore pour ignorer la data

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import json
 config = json.load(open('config.json'))
 bot = commands.Bot(command_prefix=config['prefix'])
 
+ff1.Cache.enable_cache('./data/cache')
+#Pour les tests, le GP utilisé est celui de Bahrein de l'année dernière
+session = ff1.get_session(2021, 1)
+print(f'Session chargée : {session.name}')
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} Connecté à Discord')
